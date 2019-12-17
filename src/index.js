833b11b74d1f4fc5091f0bbd5fb5a08f49c3c2f6
antonstrand <jag@antonstrand.se>
2019-12-17 17:09:42 +0100

    Read in csv and convert to expected input

--- src/index.js
+++ src/index.js
@@ -1,1 +1,58 @@
-console.log('hello world')
+'use strict'
+const Async = require('crocks/Async')
+const Pair = require('crocks/Pair')
+const not = require('crocks/logic/not')
+const {
+  append,
+  filter,
+  init,
+  isEmpty,
+  isNil,
+  last,
+  map,
+  pipe,
+  reduce,
+  split,
+  tail
+} = require('ramda')
+const fs = require('fs')
+
+let categoryIndex = {}
+let size = 0
+
+const categoryToInt = category => {
+  if (!isNil(categoryIndex[category])) {
+    return categoryIndex[category]
+  } else {
+    categoryIndex[category] = size++
+    return categoryIndex[category]
+  }
+}
+
+/** readFile :: String -> Async Error String */
+const readFile = path => Async.fromNode(fs.readFile)(path, 'utf8')
+
+/** lines :: String -> [String] */
+const lines = split('\n')
+
+/** columns :: String -> [String] */
+const columns = split(',')
+
+/** removeEmptyRows :: [a] -> [a] */
+const removeEmptyRows = filter(not(isEmpty))
+
+const convertCSV = pipe(lines, tail, removeEmptyRows, map(columns))
+
+/** readCSV :: String -> [[a]] */
+const readCSV = pipe(readFile, map(convertCSV))
+
+/** splitDataAndCategories :: [[a]] -> Pair [[a]] [Number] */
+const splitDataAndCategories = reduce(
+  (prev, row) =>
+    prev.bimap(append(init(row)), append(categoryToInt(last(row)))),
+  Pair([], [])
+)
+
+readCSV('./datasets/iris.csv')
+  .map(splitDataAndCategories)
+  .fork(console.error, x => map(console.log, x.fst()))
