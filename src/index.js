087250925d9cd02ef4651ffce12bd6f97d8a2089
Anton Strand <jag@antonstrand.se>
2019-12-21 02:42:05 +0100

    Able to train and predict

--- src/index.js
+++ src/index.js
@@ -1,46 +1,41 @@
 'use strict'
 const { readData } = require('./readData')
 const { categoryToInt } = require('./readData/categoryToInt')
+const NaiveBayes = require('./model/NaiveBayes')
 
-const { map, mean, pipe, propOr, transpose } = require('ramda')
+/** matrix :: [a] -> [[ Number ]] */
+const matrix = base => base.map(() => Array(base.length).fill(0))
 
-/** categorize :: ([[ Number ]], [ Number ]) -> Object */
-const categorize = (data, categories) =>
-  data.reduce(
-    (categorized, row, i) => ({
-      ...categorized,
-      [categories[i]]: [...propOr([], categories[i], categorized), row]
-    }),
-    {}
-  )
+/** confusion_matrix :: ([Number], [Number]) -> [[ Number ]] */
+const confusion_matrix = (X, y) => {
+  const mtx = matrix([...new Set(y)])
 
-/** standardDeviation :: [ Number ] -> Number */
-const standardDeviation = ns => {
-  const m = mean(ns)
-  return Math.sqrt(
-    ns.reduce((sum, n) => sum + (n - m) * (n - m), 0) / ns.length
-  )
+  for (let i = 0; i < X.length; i++) {
+    mtx[y[i]][X[i]] = mtx[y[i]][X[i]] + 1
+  }
+  return mtx
 }
 
-/** summarizeAttribute :: [ Number ] -> { mean :: Number, stdDev :: Number } */
-const summarizeAttribute = ns => ({
-  mean: mean(ns),
-  stdDev: standardDeviation(ns)
-})
-
-/** train :: ([[ Number ]], [ Number ]) -> [[ { mean :: Number, stdDev :: Number } ]] */
-const train = pipe(
-  categorize,
-  Object.values,
-  map(pipe(transpose, map(summarizeAttribute)))
-)
-
 readData(categoryToInt())('./datasets/iris.csv').fork(console.error, x => {
-  // map(console.log, x.fst())
-  console.log(train(x.fst(), x.snd()))
+  console.time('Iris')
+  const irises = new NaiveBayes()
+  console.time('Iris - Training time')
+  irises.fit(x.fst(), x.snd())
+  console.timeEnd('Iris - Training time')
+  console.time('Iris - Evaluation time')
+  console.table(confusion_matrix(irises.predict(x.fst()), x.snd()))
+  console.timeEnd('Iris - Evaluation time')
+  console.timeEnd('Iris')
 })
 
-// readData(categoryToInt())('./datasets/banknote.csv').fork(console.error, x => {
-//   // map(console.log, x.fst())
-//   console.log(x.snd())
-// })
+readData(categoryToInt())('./datasets/banknote.csv').fork(console.error, x => {
+  console.time('Notes')
+  const notes = new NaiveBayes()
+  console.time('Notes - Training time')
+  notes.fit(x.fst(), x.snd())
+  console.timeEnd('Notes - Training time')
+  console.time('Notes - Evaluation time')
+  console.table(confusion_matrix(notes.predict(x.fst()), x.snd()))
+  console.timeEnd('Notes - Evaluation time')
+  console.timeEnd('Notes')
+})
--- src/model/NaiveBayes.js
+++ src/model/NaiveBayes.js
@@ -1,5 +1,57 @@
 'use strict'
+const { map, mean, pipe, propOr, sum, transpose } = require('ramda')
+const { standardDeviation } = require('../utils')
+
+/** categorize :: ([[ Number ]], [ Number ]) -> Object */
+const categorize = (data, categories) =>
+  data.reduce(
+    (categorized, row, i) => ({
+      ...categorized,
+      [categories[i]]: [...propOr([], categories[i], categorized), row]
+    }),
+    {}
+  )
+
+/** summarizeAttribute :: [ Number ] -> { mean :: Number, stdDev :: Number } */
+const summarizeAttribute = ns => ({
+  mean: mean(ns),
+  stdDev: standardDeviation(ns)
+})
+
+/** train :: ([[ Number ]], [ Number ]) -> [[ { mean :: Number, stdDev :: Number } ]] */
+const train = pipe(
+  categorize,
+  Object.values,
+  map(pipe(transpose, map(summarizeAttribute)))
+)
+
+/** normalize :: [Number] -> [Number] */
+const normalize = ns => ns.map(n => n / sum(ns))
+
+/** getCategory :: [Number] -> Number */
+const getCategory = ns => ns.indexOf(Math.max(...ns))
+
+/** pdf :: (Number, Number, Number) -> Number */
+const pdf = (x, mean, stdDev) =>
+  (1 / (Math.sqrt(2 * Math.PI) * stdDev)) *
+  Math.E ** (-((x - mean) ** 2) / (2 * stdDev ** 2))
 
 class NaiveBayes {
-  train (X, y) {}
+  fit (X, y) {
+    this._categories = train(X, y)
+  }
+
+  predict (X) {
+    return X.map(row =>
+      this._categories.map(attributes =>
+        Math.exp(
+          row
+            .map((x, i) => pdf(x, attributes[i].mean, attributes[i].stdDev))
+            .reduce((sum, x) => sum + Math.log(x), 0)
+        )
+      )
+    ).map(pipe(normalize, getCategory))
+  }
 }
+
+module.exports = NaiveBayes
